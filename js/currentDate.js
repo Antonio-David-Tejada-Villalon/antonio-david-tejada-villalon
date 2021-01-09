e5db311ab3d6604cf7a8dd0fe2d1143a0ef539ce
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2021-01-09 12:15:37 -0300

    W01 ✅ Assignment: Course Portal Home Page

--- js/currentDate.js
+++ js/currentDate.js
@@ -19,11 +19,12 @@ const months = [
 	"October",
 	"November",
 	"December",];
-const todaysdate = new Date();
+
 const dayNames = daynames[todaysdate.getDay()];
 const monthNames = months[todaysdate.getMonth()];
 const currentdate = dayNames + ", " + todaysdate.getDate() + " " + monthNames + ", " + todaysdate.getFullYear();
-document.getElementById("currentdate").textContent = currentdate;
-document.getElementById("currentyear").textContent = todaysdate.getFullYear();*/
+document.getElementById("currentdate").textContent = currentdate;*/
+const todaysdate = new Date();
+document.getElementById("currentyear").textContent = todaysdate.getFullYear();
 const lastmodification = document.querySelector('#time');
 lastmodification.textContent = document.lastModified;
