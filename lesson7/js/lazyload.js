2ca811361044924e47614b1dbd459725f56522f8
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2021-02-19 16:26:35 -0300

    W07 ✅ Assignment: The Gallery Page

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,37 +1,3 @@
-//get all imgs with data-src attribute
-/*const imagesToLoad = document.querySelectorAll("img[data-src]");
-
-//optional parameters being set for the IntersectionalObserver 
-const imgOptions = {
-    threshold: 0;
-    rootMargin: "0px 0px 50px 0px";
-};
-
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => { image.removeAttribute('data-src');};
-};
-
-//first check to see if Intersection Observer is supported
-
-if ("IntersectionObserver" in window) {
-     const observer = new IntersectionObserver((items, observer) => {
-         items.forEach((item) => {
-           if (item.isIntersecting) {
-             loadImages(item.target);
-             observer.unobserve(item.target);
-           }
-         });
-       });
-       imagesToLoad.forEach((img) => {
-         observer.observe(img);
-       });
-     } else {
-       imagesToLoad.forEach((img) => {
-         loadImages(img);
-       });
-     }*/
-
 //Get all the images with data-src attribute
 const images = document.querySelectorAll("[data-src]");
 
@@ -42,12 +8,13 @@ function preloadImage(img) {
   }
 
   img.src = src;
+  img.removeAttribute("data-src");
 }
 
 //optional parameters being set for the IntersectionalObserver
 const imgOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px 50px 0px",
+  threshold: 0,
+  rootMargin: "0px 0px 300px 0px",
 };
 
 //loop through the images and determine which ones need to be shown
