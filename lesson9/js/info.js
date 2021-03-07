c3d2aa69c488de7e860b0a7f6eb590295059b863
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2021-03-07 00:12:02 -0300

    W09 ✅ Assignment: The Weather Site Home Page

--- lesson9/js/info.js
+++ lesson9/js/info.js
@@ -9,18 +9,39 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const towns = jsonObject["towns"];
     for (let i = 0; i < towns.length; i++) {
-      let data = document.createElement("section");
-      let h2 = document.createElement("h2");
+      let dat = document.createElement("section");
       let art = document.createElement("article");
+      let h2 = document.createElement("h2");
+      let p1 = document.createElement("p");
+      let p2 = document.createElement("p");
+      let p3 = document.createElement("p");
+      let p4 = document.createElement("p");
       let image = document.createElement("img");
 
-      h2.textContent = towns[i].name + " " + towns[i].photo;
-      image.setAttribute("src", towns[i].photo);
-      //image.setAttribute("alt", prophets[i].name + " " + prophets[i].lastname);
+      dat.setAttribute('class', 'info');
+      art.setAttribute('class', 'arcle');
+      h2.setAttribute('class', 'htitle');
+      h2.textContent = towns[i].name;
+      p1.setAttribute('class', 'para');
+      p1.textContent = towns[i].motto;
+      p2.setAttribute('class', 'para');
+      p2.textContent = "Year Founded:" + " " + towns[i].yearFounded;
+      p3.setAttribute('class', 'para');
+      p3.textContent = "Current Population:" + " " + towns[i].currentPopulation;
+      p4.setAttribute('class', 'para');
+      p4.textContent = "Average RainFall:" + " " + towns[i].averageRainfall;
+      image.setAttribute('class', 'imgt');
+      image.setAttribute("src", `images/${towns[i].photo}`);
+      image.setAttribute("alt", towns[i].name);
 
-      data.appendChild(h2);
-      //data.appendChild(image);
+      art.appendChild(h2);
+      art.appendChild(p1);
+      art.appendChild(p2);
+      art.appendChild(p3);
+      art.appendChild(p4);
+      dat.appendChild(art);
+      dat.appendChild(image);
 
-      document.querySelector("div.data").appendChild(data);
+      document.querySelector("div.dat").appendChild(dat);
     }
   });
