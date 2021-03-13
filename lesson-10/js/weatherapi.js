220e77984ad0817c52570a015dcaa7607833fee4
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2021-03-13 01:44:46 -0300

    W10 ✅ Assignment: Preston Weather API

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL =
+/*const apiURL =
   "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=f2d1a5e40996e4db6f64e25ba3b67c8f";
 fetch(apiURL)
   .then((response) => response.json())
@@ -11,4 +11,18 @@ fetch(apiURL)
     document.getElementById("icon").setAttribute("src", imagesrc); // focus on the setAttribute() method
     document.getElementById("icon").setAttribute("alt", desc);
     document.getElementById("current-temp").textContent = temp.toFixed(2);
+  });*/
+
+  const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=f2d1a5e40996e4db6f64e25ba3b67c8f&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const imagesrc = "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png"; // note the concatenation
+    const desc = jsObject.weather[0].description; // note how we reference the weather array
+    document.getElementById("imagesrc").textContent = imagesrc; // informational specification only
+    document.getElementById("icon").setAttribute("src", imagesrc); // focus on the setAttribute() method
+    document.getElementById("icon").setAttribute("alt", desc);
+    document.getElementById("current-temp").textContent = jsObject.main.temp;
   });
